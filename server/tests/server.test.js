6570269d0511c8e6bc21bc11a814b508bbd7649f
c26842684 <d26842684@hotmail.com>
2017-03-19 14:12:36 -0400

    test Get /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,9 +5,19 @@ const {app} = require("./../server.js");
 const {Todo} = require("./../models/todo.js");
 const {User} = require("./../models/user.js");
 
+const dummyTodos = [{
+  text: "first todo"
+},{
+  text: "second todo"
+}];
+
+
 beforeEach((done)=>{
   Todo.remove({}).then(()=>{
-    done();
+    Todo.insertMany(dummyTodos).then(()=>{
+      done();
+    });
+
   });
 
 });
@@ -20,13 +30,13 @@ describe("Post /todos",()=>{
       .post("/todos")
       .send({text})
       .expect(200)
-      .expect((res)=>{
+      .expect((res)=>{    // customer assertion
         expect(res.body.text).toBe(text);
       })
       .end((err,res)=>{
         if(err) return done(e);
 
-        Todo.find().then((todos)=>{
+        Todo.find({text}).then((todos)=>{
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -44,10 +54,27 @@ describe("Post /todos",()=>{
       .end((err,res)=>{
         if(err) return done(err);
         Todo.find().then((todos)=>{
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         });
       });
   });
+});
 
+describe("Get /todos",()=>{
+  it("succeed to get todos",(done)=>{
+    request(app)
+      .get("/todos")
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body.todos.length).toBe(2);
+        expect(res.body.todos[0].text).toBe("first todo");
+        expect(res.body.todos[1].text).toBe("second todo");
+      })
+      .end((err,res)=>{
+        if(err)
+          return done(err);
+        done();
+      });
+  });
 });
